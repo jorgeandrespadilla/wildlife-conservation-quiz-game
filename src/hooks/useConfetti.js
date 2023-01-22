8b7cce4714d7acb064f2e5f950d72887c887362a
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-01-22 15:19:48 -0500

    Add useConfetti hook

---
+++ src/hooks/useConfetti.js
@@ -0,0 +1,73 @@
+import { useCallback, useRef } from "react";
+
+const canvasStyles = {
+  position: "fixed",
+  pointerEvents: "none",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0
+};
+
+export const useConfetti = () => {
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio)
+      });
+  }, []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55
+    });
+
+    makeShot(0.2, {
+      spread: 60
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45
+    });
+  }, [makeShot]);
+
+  const fireCount = useCallback((count = 1) => {
+    for (let i = 0; i < count; i++) {
+      setTimeout(() => {
+        fire();
+      }, i * 300);
+    }
+  }, [fire]);
+
+  return {
+    /** Get the instance of the confetti animation. */
+    getInstance,
+    /** Styles for the canvas. */
+    canvasStyles,
+    /** Fire the confetti animation N times. By default, it fires once. */
+    fire: fireCount,
+  };
+};
